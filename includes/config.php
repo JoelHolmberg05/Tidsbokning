bd2bc5b490934c29cb329f10f5c2930bfbb291d1
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-04 13:14:47 +0200

    made the files

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+	$servername = "127.0.0.1";
+	$username = "root";
+	$password = "";
+
+	try {
+	  $conn = new PDO("mysql:host=$servername;dbname=2022_form;charset=utf8mb4", $username, $password);
+	  // set the PDO error mode to exception
+	  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  echo "Connected successfully<br>";
+	} catch(PDOException $e) {
+	  echo "Connection failed: " . $e->getMessage();
+	}
+	
+	
+?>
