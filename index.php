355d8f68041d94ec09e5e9fbdf49021f1357f299
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-07 15:20:59 +0200

    tried to make a table

--- index.php
+++ index.php
@@ -6,13 +6,33 @@
     <title>Schema</title>
 </head>
 <body>
-    
+
+<table id="table" style="width:100%">
+  <tr>
+    <th>Datum</th>
+    <th>Tid</th>
+    <th>Massage typ</th>
+    <th>Namn</th>
+    <th>Telefonnummer</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+
 </body>
 </html>
 
 
 <?php
 
-    echo "<p>test</p>"
+    
 
 ?>
