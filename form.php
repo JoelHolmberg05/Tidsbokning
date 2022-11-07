4686e08d0ca7e6b12c8b5475d6825936fadcc746
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-07 13:53:23 +0200

    edited some css

--- form.php
+++ form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Document</title>
+    <title>Tidsbokning</title>
 </head>
 <body>
 
@@ -11,24 +11,25 @@
     <form method="post" action="index2.php">
 
         <label for="Date">Datum</label><br>
-        <input type="text" id="Date" name="Date"><br><br>
+        <input type="text" id="date" name="Date"><br><br>
 
         <label for="Time">Tid</label><br>
-        <input type="text" id="Time" name="Time"><br><br>
+        <input type="text" id="time" name="Time"><br><br>
 
         <label for="MassageType">Massage typ</label><br>
-        <input type="text" id="MassageType" name="MassageType"><br><br>
+        <input type="text" id="massageType" name="MassageType"><br><br>
 
         <label for="Name">Namn</label><br>
-        <input type="text" id="Name" name="Name"><br><br>
+        <input type="text" id="name" name="Name"><br><br>
 
         <label for="PhoneNmr">Telefonnummer</label><br>
-        <input type="text" id="PhoneNmr" name="PhoneNmr"><br><br>
+        <input type="text" id="phoneNmr" name="PhoneNmr"><br><br>
 
 
         <input type="submit" name="submitbtn" id="submitbtn" value="Submit">
     </form>
 </div>
+
 </body>
 </html>
 
--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@
 	  $conn = new PDO("mysql:host=$servername;dbname=2022_form;charset=utf8mb4", $username, $password);
 	  // set the PDO error mode to exception
 	  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	  echo "Connected successfully<br>";
+	  //echo "Connected successfully<br>";
 	} catch(PDOException $e) {
 	  echo "Connection failed: " . $e->getMessage();
 	}
