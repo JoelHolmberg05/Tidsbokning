bce3d1d2345122e9e95472a7423d96581a1ec8aa
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-07 13:27:36 +0200

    did smth i think

--- form.php
+++ form.php
@@ -5,10 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Tidsbokning</title>
+    <title>Document</title>
 </head>
 <body>
+
 <div>
+    <form method="post" action="index2.php">
+
+        <label for="Date">Datum</label><br>
+        <input type="text" id="Date" name="Date"><br><br>
+
+        <label for="Time">Tid</label><br>
+        <input type="text" id="Time" name="Time"><br><br>
+
+        <label for="MassageType">Massage typ</label><br>
+        <input type="text" id="MassageType" name="MassageType"><br><br>
+
+        <label for="Name">Namn</label><br>
+        <input type="text" id="Name" name="Name"><br><br>
+
+        <label for="PhoneNmr">Telefonnummer</label><br>
+        <input type="text" id="PhoneNmr" name="PhoneNmr"><br><br>
+
+
+        <input type="submit" name="submitbtn" id="submitbtn" value="Submit">
+    </form>
+</div>
+</body>
+</html>
 
     <form method="post" action="index2.php">
         <label for="Date">Datum</label><br>
@@ -30,9 +54,6 @@
         <input type="submit" name="submitbtn" id="submitbtn" value="Submit">
 </form>
 </div>
-
-</body>
-</html>
 
 
 <?php
