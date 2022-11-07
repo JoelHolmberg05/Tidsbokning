252e02cfc36404013a054661fe3c10af37fbdabb
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-07 13:32:32 +0200

    changed names on the files and forgot about it and tried to fix smth he thought he broke

--- form.php
+++ form.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <title>Document</title>
 </head>
@@ -33,27 +31,6 @@
 </div>
 </body>
 </html>
-
-    <form method="post" action="index2.php">
-        <label for="Date">Datum</label><br>
-        <input type="text" id="Date" name="Date"><br><br>
-
-        <label for="Time">Tid</label><br>
-        <input type="text" id="Time" name="Time"><br><br>
-
-        <label for="MassageType">Massage typ</label><br>
-        <input type="text" id="MassageType" name="MassageType"><br><br>
-
-        <label for="Name">Namn</label><br>
-        <input type="text" id="Name" name="Name"><br><br>
-
-        <label for="PhoneNmr">Telefonnummer</label><br>
-        <input type="text" id="PhoneNmr" name="PhoneNmr"><br><br>
-
-
-        <input type="submit" name="submitbtn" id="submitbtn" value="Submit">
-</form>
-</div>
 
 
 <?php
