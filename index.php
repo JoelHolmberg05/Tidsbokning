0233138cf5477bba585cdcd2ffcfe0eb60f899a3
Joel Holmberg <joel.holmberg@axxell.fi>
2022-11-07 15:01:11 +0200

    made changes

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Schema</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
 <?php
+
+    echo "<p>test</p>"
 
 ?>
--- navpage.php
+++ navpage.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Navpage</title>
+</head>
+<body>
+
+    <a class="link" href="form.php">Form</a><br><br>
+    <a class="link" href="index.php">Schema</a>
+
+</body>
+</html>
+
 <?php
 
-
+    
 
 ?>
